c4f031c239d1daddd6da164a95084f65b5bc9e05
Timur Karimov <tationkar@gmail.com>
2023-05-23 21:26:18 +0300

    fourth

---
+++ Sen2_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// 16 Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num * num == num2 || num2 * num2 == num)
+    Console.WriteLine($"{num}, {num2} Да");
+else
+    Console.WriteLine($"{num}, {num2} Нет");
