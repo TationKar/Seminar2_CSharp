4988e46b35c01ec9fd5183a2182966b4dad72274
Timur Karimov <tationkar@gmail.com>
2023-05-23 21:12:15 +0300

    third task

---
+++ Sem2_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*12 Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если второе число не кратно числу первому,
+то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4  -> кратно
+*/
+Console.Clear();
+Console.Write("Введите число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int scndNum = Convert.ToInt32(Console.ReadLine());
+int result = firstNum % scndNum;
+if (result == 0) 
+Console.WriteLine($"{firstNum}, {scndNum} -> кратно");
+else    
+Console.WriteLine($"{firstNum}, {scndNum} -> не кратно, остаток {result}");
---
+++ Sem2_3/Program.cs
@@ -0,0 +1,23 @@
+﻿/ **Задача 14:**
+
+// Напишите программу, которая принимает на вход число и проверяет,
+//  кратно ли оно одновременно 7 и 23.
+//if((a > b || a < c) && (x > b || x < c) || d == 5))
+//     0    +    0          1    +   0
+ //         0          *          1
+ //                    0                   +   1
+ //                                1    
+// 14  ->  нет
+// 46  ->  нет
+// 161 ->  да
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num % 7;
+int num3 = num % 23;
+if((num2 == 0) && (num3 == 0))
+{
+    Console.WriteLine("кратно");
+}
+else
+    Console.WriteLine("не кратно");
