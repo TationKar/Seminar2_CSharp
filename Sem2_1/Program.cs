5d5bf85a708ae8cacd26241817119916a7968dbb
Timur Karimov <tationkar@gmail.com>
2023-05-23 20:27:07 +0300

    delete middle number

--- Sem2_1/Program.cs
+++ Sem2_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//11 Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int numHundred = num / 100;
+int numOne = num % 10;
+Console.WriteLine($"Случайное трехзначное число {num}");
+int result = numHundred * 10 + numOne;
+
+Console.WriteLine($"Результат {result}");
